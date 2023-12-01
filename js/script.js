2273816657ea0ac7cfa653e29e95bd3348a8a4de
imLukePex <luca.pessione@gmail.com>
2023-12-01 16:03:37 +0100

    creating arrayMembri

---
+++ js/script.js
@@ -0,0 +1,43 @@
+//  Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+// Creazione Array di oggetti dei membri del team
+const arrayMembri = [
+    {
+        "nome": "Wayne",
+        "cognome": "Barnett",
+        "role": "Founder & CEO",
+        "img": "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        "nome": "Angela",
+        "cognome": "Caroll",
+        "role": "Chief Editor",
+        "img": "angela-caroll-chief-editor.jpg"
+    },
+    {
+        "nome": "Walter",
+        "cognome": "Gordon",
+        "role": "Office Manager",
+        "img": "walter-gordon-office-manager.jpg"
+    },
+    {
+        "nome": "Angela",
+        "cognome": "Lopez",
+        "role": "Social Media Manager",
+        "img": "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        "nome": "Scott",
+        "cognome": "Estrada",
+        "role": "Developer",
+        "img": "scott-estrada-developer.jpg"
+    },
+    {
+        "nome": "Barbara",
+        "cognome": "Ramos",
+        "role": "Graphic Designer",
+        "img": "barbara-ramos-graphic-designer.jpg"
+    }
+]
+
+// Ciclare su tutti i membri e stampare per ognuno di essi: nome, cognome, ruolo e immagine.
